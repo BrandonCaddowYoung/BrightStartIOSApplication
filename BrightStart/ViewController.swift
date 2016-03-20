efeb935a92666c2a18de5d5d7d1a5893241bb940
dev <dev@devs-MacBook-Pro.local>
2016-03-20 15:09:26 +0000

    Initial Commit

---
+++ BrightStart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BrightStart
+//
+//  Created by dev on 20/03/2016.
+//  Copyright © 2016 dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
